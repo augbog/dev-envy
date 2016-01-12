3028867c6be339b89fe8892cb53932b88626599d
Augustus Yuan <ayuan@evernote.com>
2016-01-11 20:00:03 -0800

    Converted Gulpfile to ES6
    - added sample files to demonstrate how to use
    - replaced gulp-minify-css with gulp-cssnano

---
+++ Gulpfile.babel.js
@@ -0,0 +1,110 @@
+'use strict';
+
+import gulp          from 'gulp';
+import minifyCSS     from 'gulp-cssnano';
+import sass          from 'gulp-sass';
+import browserSync   from 'browser-sync';
+import uglify        from 'gulp-uglify';
+import rename        from 'gulp-rename';
+import replace       from 'gulp-replace';
+import notify        from 'gulp-notify';
+import beep          from 'beepbeep';
+import colors        from 'colors';
+import plumber       from 'gulp-plumber';
+import path          from 'path';
+import eslint        from 'gulp-eslint';
+import sourcemaps    from 'gulp-sourcemaps';
+import source        from 'vinyl-source-stream';
+import buffer        from 'vinyl-buffer';
+import browserify    from 'browserify';
+import watchify      from 'watchify';
+import babel         from 'babelify';
+
+function compile(watch) {
+  let bundler = browserify({
+    // Define the entry point for our application
+    entries: ['./js/main.es6.js'],
+    // Debugging is nice
+    debug: true,
+    // Allow importing from the following extensions
+    extensions: [' ', 'js', 'jsx']
+  }).transform(babel.configure({
+    presets: ["es2015"]
+  }));
+
+  if (watch) {
+    bundler = watchify(bundler);
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('main.js'))
+      .pipe(buffer())
+      .pipe(uglify())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./build/js'));
+  }
+
+  rebundle();
+}
+
+function watch() {
+  return compile(true);
+};
+
+// error handling convenience wrapper
+gulp.plumbedSrc = function(){
+  return gulp.src.apply(gulp, arguments)
+    .pipe(plumber({
+      errorHandler: function(err) {
+        beep();
+        console.log('[ERROR:]'.bold.red);
+        console.log(err.messageFormatted);
+        this.emit('end');
+      }
+    }));
+};
+
+gulp.task('sass', () => {
+  return gulp.plumbedSrc('sass/**/*.scss')
+    .pipe(sass())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./build/css/'))
+    .pipe(notify({ message: 'CSS complete' }));
+});
+
+gulp.task('browser-sync', () => {
+  browserSync({
+    server: {
+      baseDir: path.join(__dirname, 'build')
+    },
+    host: 'localhost',
+    port: 8000,
+    open: false
+  });
+});
+
+gulp.task('scripts', () => {
+  return compile();
+});
+
+gulp.task('eslint', () => {
+  return gulp.plumbedSrc('./js/**/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .pipe(notify({ message: 'ESLint complete' }));
+});
+
+gulp.task('watch', ['browser-sync'], () => {
+  gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch('js/**/*.js', ['eslint', 'scripts']);
+});
+
+gulp.task('default', ['watch']);
---
+++ js/main.es6.js
@@ -0,0 +1,8 @@
+// Sample ES6 code
+// This file will get transpiled out into /build/js
+
+// String interpolation
+const name = 'Bob';
+const time = 'Rawr';
+const sentence = `Hello ${name}, how are you ${time}?`;
+console.log(sentence);
